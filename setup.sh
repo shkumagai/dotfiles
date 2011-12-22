b0d684fe3208e0dcdb63c0ec87c8fd22337cd089
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2011-12-23 01:28:32 +0900

    Update setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,67 @@
 #!/bin/bash
 
-## *rc (resource) files
-for rcfile in ?*rc
-do
-    ln -Fis "$PWD/$rcfile" "$HOME/.${rcfile}"
+# setup.sh -- setting up dotfiles to home directory.
+#
+# :author: Shoji KUMAGAI
+# :since: 
 
-    archtype=$(uname -s | tr '[:upper:]' '[:lower:]')
-    subrcfile="$rcfile.$archtype"
-    if [[ -f $subrcfile ]]
-    then
-        ln -Fis "$PWD/$subrcfile" "$HOME/.$rcfile.local"
-    fi
+# usage
+# -----
+# Just do as below:
+#
+# $ ./setup.sh
+#
+# symbolic link naming rule
+# -------------------------
+# xxx         --> $HOME/.xxx
+# xxx.darwin  --> $HOME/.xxx.local
+#
+# The file that has suffix like `darwin` used for configuration in certain
+# sysmem. In the case above, `xxx.darwin` is used to configuration of
+# application like Shell (ex. bash, zsh), VCS (ex. mercurial, git), and so
+# on. Of course, dotfiles should be designed in which it has interfaces to
+# include the system inherent configuration.  
+
+# Here is a sample code for zshrc:
+#
+#  [[ -f $HOME/.zshrc.local ]] && source $HOME/.zshrc.local
+
+
+make_symlink()
+{
+  #echo "args: [$*]"
+  file=$1
+  suffix=$2
+
+  [[ "$file" = "" ]] && exit 0
+
+  if [[ "$suffix" != "" ]]; then
+    ln -Fis "$PWD/$file" "$HOME/.${file%.?*}$suffix"
+  else
+    ln -Fis "$PWD/$file" "$HOME/.${file}"
+  fi
+}
+
+make_symlink_local()
+{
+  make_symlink $1 '.local'
+}
+
+for f in `find . -depth 1 -type f`
+do
+  name="${f##?*/}" # triming
+  arch=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+  if [[ "${f##?*rc}" = "" ]]; then
+    make_symlink $name
+    [[ -f "$name.$arch" ]] && make_symlink_local $name.$arch
+  fi
+  if [[ "${f##?*config}" = "" ]]; then
+    make_symlink $name
+  fi
+  if [[ "${f##?*ignore}" = "" ]]; then
+    make_symlink $name
+  fi
 done
 
-## *config files
-for conffile in ?*config
-do
-    ln -Fis "$PWD/$conffile" "$HOME/.$conffile"
-done
-
-## *ignore files
-for ignorefile in ?*ignore
-do
-    ln -Fis "$PWD/$ignorefile" "$HOME/.$ignorefile"
-done
+# END
