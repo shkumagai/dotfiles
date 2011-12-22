e89b8fe6f33ae9b16a76a97f7e20c4eaf9fba6c8
Shoji KUMAGAI <take.this.2.your.grave@gmail.com>
2011-12-22 22:50:33 +0900

    Add VCS configuration and ignore files.

--- setup.sh
+++ setup.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+## *rc (resource) files
 for rcfile in ?*rc
 do
-    ln -Fis "$PWD/${rcfile}" "$HOME/.${rcfile}"
+    ln -Fis "$PWD/$rcfile" "$HOME/.${rcfile}"
 
     archtype=$(uname -s | tr '[:upper:]' '[:lower:]')
-    subrcfile="${rcfile}.${archtype}"
-    if [[ -f ${subrcfile} ]]
+    subrcfile="$rcfile.$archtype"
+    if [[ -f $subrcfile ]]
     then
-        ln -Fis "$PWD/${subrcfile}" "$HOME/.${rcfile}.local"
+        ln -Fis "$PWD/$subrcfile" "$HOME/.$rcfile.local"
     fi
 done
 
+## *config files
 for conffile in ?*config
 do
-    ln -Fis "$PWD/${conffile}" "$HOME/.${conffile}"
+    ln -Fis "$PWD/$conffile" "$HOME/.$conffile"
+done
+
+## *ignore files
+for ignorefile in ?*ignore
+do
+    ln -Fis "$PWD/$ignorefile" "$HOME/.$ignorefile"
 done
