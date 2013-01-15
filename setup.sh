9078ecc63ab4f766d203769a21a70e460052c414
Shoji KUMAGAI <kumagai@accense.com>
2013-01-15 12:17:18 +0900

    Update some files
    - Add new functionalities to setup.sh
    - Add blank line

--- setup.sh
+++ setup.sh
@@ -5,12 +5,6 @@
 # :author: Shoji KUMAGAI
 # :since: Fri Dec 23 01:56:01 JST 2011
 
-# usage
-# -----
-# Just do as below:
-#
-# $ ./setup.sh
-#
 # symbolic link naming rule
 # -------------------------
 # xxx         --> $HOME/.xxx
@@ -26,20 +20,77 @@
 #
 #  [[ -f $HOME/.zshrc.local ]] && source $HOME/.zshrc.local
 
+# escape sequence
+#
+readonly red='0;31'
+readonly green='0;32'
+readonly brown='0;33'
+readonly blue='0;34'
+readonly gray='0;37'
+readonly org='0;0'
+
+DEBUG=0
+FORCE_UPDATE=0
+SRC=$PWD
+DEST=$HOME
+
+USAGE_MSG='usage:
+    setup.sh [option]
+
+option:
+    -d               run as debug mode
+    -f               force update symbolic link
+    -h               print help message
+    -v               print version
+'
+usage()
+{
+  echo "$USAGE_MSG" 1>&2
+}
+
+VERSION='0.2.0'
+version()
+{
+  echo "version: ${1}"
+}
+
+colored_echo()
+{
+  color="${2:-$org}"
+  echo -e "\033[${2}m${1}\033[${org}m"
+}
+
+debug()
+{
+  [ $DEBUG -ne 0 ] && colored_echo "DEBUG: ${1}" $gray
+}
+
+lc() {
+  echo -n $( echo ${1} | tr "[:upper:]" "[:lower:]" )
+}
 
 make_symlink()
 {
-  #echo "args: [$*]"
+  debug "  args: [$*]"
   file=$1
   suffix=$2
+  [ "$file" = "" ] && exit 0
 
-  [[ "$file" = "" ]] && exit 0
+  from="$SRC/$file"
 
-  if [[ "$suffix" != "" ]]; then
-    ln -Fis "$PWD/$file" "$HOME/.${file%.?*}$suffix"
+  if [ "$suffix" != "" ]; then
+    to="$DEST/.${file%.?*}$suffix"
   else
-    ln -Fis "$PWD/$file" "$HOME/.${file}"
+    to="$DEST/.${file}"
   fi
+
+  if [ $FORCE_UPDATE -eq 1 ]; then
+    LN_OPTS="-fs"
+  else
+    LN_OPTS="-is"
+  fi
+  debug "ln $LN_OPTS $from $to"
+  ln $LN_OPTS "$from" "$to"
 }
 
 make_symlink_local()
@@ -47,22 +98,64 @@ make_symlink_local()
   make_symlink $1 '.local'
 }
 
-for f in `find . -maxdepth 1 -type f`
-do
-  name="${f##?*/}" # triming
-  arch=$(uname -s | tr '[:upper:]' '[:lower:]')
+run()
+{
+  if [ $# -eq 0 ]; then
+    files=$(find . -maxdepth 1 -type f)
+  else
+    files="$@"
+  fi
+  debug "files #=> $files"
 
-  if [[ "${f##?*rc}" = "" ]]; then
-    make_symlink $name
-    [[ -f "$name.$arch" ]] && make_symlink_local $name.$arch
-  fi
-  if [[ "${f##?*config}" = "" ]]; then
-    make_symlink $name
-    [[ -f "$name.local" ]] && make_symlink_local $name.local
-  fi
-  if [[ "${f##?*ignore}" = "" ]]; then
-    make_symlink $name
-  fi
+  arch=$(lc `uname -s`)
+  debug "arch #=> $arch"
+
+  for f in $files
+  do
+    name="${f##?*/}" # triming
+    debug "name #=> $name"
+
+    if [ "${f##?*rc}" = "" ]; then
+      make_symlink $name
+      [ -f "$name.$arch" ] && make_symlink_local $name.$arch
+    fi
+    if [ "${f##?*config}" = "" ]; then
+      make_symlink $name
+      [ -f "$name.local" ] && make_symlink_local $name.local
+    fi
+    if [ "${f##?*ignore}" = "" ]; then
+      make_symlink $name
+    fi
+  done
+}
+
+### Main
+while getopts dfhv option; do
+  case "$option" in
+    d)
+      DEBUG=1
+      echo "Enable debug output."
+      ;;
+    f)
+      FORCE_UPDATE=1
+      echo "Enable symbolic links force update."
+      ;;
+    h|\?)
+      usage; exit 0
+      ;;
+    v)
+      version $VERSION; exit 0
+      ;;
+  esac
 done
+shift $(($OPTIND - 1))
 
-# END
+run "$@"
+
+# Local variables:
+# mode: shell-script
+# sh-basic-offset: 2
+# sh-indent-comment: t
+# indent-tabs-mode: nil
+# End:
+# ex: sw=2 ts=2 et filetype=sh
