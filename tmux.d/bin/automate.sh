6c0424672190158b17ea456f6516e99cf1ae4f71
shkumagai <take.this.2.your.grave@gmail.com>
2015-03-30 17:22:35 +0900

    add switch for shell

--- tmux.d/bin/automate.sh
+++ tmux.d/bin/automate.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 TEMP_PASSWD=/tmp/_passwd
+MARK="\$"
 
 source ${TEMP_PASSWD}
+
+while getopts bz option; do
+    case "$option" in
+        b) MARK="\$" ;;
+        z) MARK="%" ;;
+    esac
+done
+shift $(($OPTIND - 1))
 
 expect -c "
 set timeout 10
 spawn ssh ${1}
 expect \"Enter passphrase for key\"
 send \"${passphrase}\n\"
-expect \"%\"
+expect \"${MARK}\"
 send \"sudo su -\n\"
 expect \"Password: \"
 send \"${passwd}\n\"
