984423a26c6121408f08da1c7c21527a4dfe3a18
shkumagai <take.this.2.your.grave@gmail.com>
2024-01-15 17:54:54 +0900

    refactoring python/timezone/timezone.py

--- scripts/python/timezone/timezone.py
+++ scripts/python/timezone/timezone.py
@@ -1,38 +1,37 @@
-# -*- coding: utf-8 -*-
 from datetime import datetime
-
-import pytz
-
-
-fmt = '%Y-%m-%d %H:%M:%S %z(%Z)'
+from typing import TypeAlias
+from zoneinfo import ZoneInfo, available_timezones
 
 
-def dump(year, mon, day, hour, min, sec, tz):
-    dt = datetime(year, mon, day, hour, min, sec, tzinfo=pytz.utc)
-    print(dt.astimezone(tz).strftime(fmt))
+fmt = '%Y-%m-%d %H:%M:%S(%Z)'
+
+TzMap: TypeAlias = dict[int, tuple[str, str]]
+
+
+def _dump(tz_map: TzMap) -> None:
+    for offset in sorted(tz_map.keys()):
+        # print(f"{offset}")
+        h = offset // 3600.0
+        m = (offset % 3600.0) / 60
+        print(f"UTC{h:-3.0f}:{m:02.0f}")
+        for zone, timestamp in sorted(tz_map[offset]):
+            print(f"\t{zone:<32} {timestamp:<30}")
 
 
 def main():
-    # pst = pytz.timezone("US/Pacific")
-    # pst_ambiguous = datetime(2018, 3, 11, 2, 59)
+    ambiguous_dt = datetime(2018, 10, 28, 1, 59, tzinfo=ZoneInfo("UTC"))
 
-    # pst_dt1 = pst.localize(pst_ambiguous, is_dst=True)
-    # print(pst_dt1.strftime(fmt))
-    # pst_dt2 = pst.localize(pst_ambiguous, is_dst=False)
-    # print(pst_dt2.strftime(fmt))
-    # pst_dt3 = pst.localize(pst_ambiguous, is_dst=None)
+    tz_map: TzMap = {}
+    for zone in available_timezones():
+        tz = ZoneInfo(zone)
+        new_dt = ambiguous_dt.astimezone(tz=tz)
+        timestamp = new_dt.strftime(fmt)
+        offset = new_dt.utcoffset().total_seconds()
 
-    ist = pytz.timezone("Israel")
-    ist_ambiguous = datetime(2018, 3, 23, 2, 59)
+        items = tz_map.setdefault(offset, set())
+        items.add((zone, timestamp))
 
-    ist_dt1 = ist.localize(ist_ambiguous, is_dst=True)
-    print(ist.utcoffset(ist_ambiguous, is_dst=True))
-    print(ist_dt1.strftime(fmt))
-    ist_dt2 = ist.localize(ist_ambiguous, is_dst=False)
-    print(ist.utcoffset(ist_ambiguous, is_dst=False))
-    print(ist_dt2.strftime(fmt))
-
-    # ist_dt3 = ist.localize(ist_ambiguous, is_dst=None)
+    _dump(tz_map=tz_map)
 
 
 if __name__ == "__main__":
