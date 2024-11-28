ef82b7a62b1e7888a1e6793f033bb74ac38560c2
shkumagai <take.this.2.your.grave@gmail.com>
2024-11-28 17:33:46 +0900

    refactoring contribution.sh

--- scripts/shell/contribution.sh
+++ scripts/shell/contribution.sh
@@ -17,8 +17,8 @@ function pb_()  { printf "$(tty_mkbold "${2}")%s${tty_reset}" "${1}"; }
 function pl_()  { printf "$(tty_escape "${2}")%s${tty_reset}\n" "${1}"; }
 function pbl_() { printf "$(tty_mkbold "${2}")%s${tty_reset}\n" "${1}"; }
 
-function pl_g() { pl_ "${1}" 32; }
-function pbl_g() { pbl_ "${1}" 32; }
+function pl_c() { pl_ "${1}" 36; }
+function pbl_c() { pbl_ "${1}" 36; }
 
 # 構成ファイル
 CONFIG_VISASQ_DIR="${HOME}/.config/visasq"
@@ -32,43 +32,43 @@ function dump_pull_requests() {
   REPOS=$(gh repo list visasq --limit 200 --json name --jq '.[]|.name' | sort)
   mkdir -p pullrequests
   for repo in ${REPOS}; do
-    printf "<%s>\n" "${repo}"
     gh pr list \
         --author "@me" \
         --state "all" \
         --limit 200 \
         --repo visasq/"${repo}" \
-        --json number,title,state,createdAt,closedAt \
+        --json number,title,state,createdAt,closedAt,additions,deletions \
         --jq "[.[] | .repo = \"${repo}\"]" \
-    > "pullrequests/${repo}.json"
-    sleep 1.5
+    > "pullrequests/${repo}.json" &
+    sleep 0.2
   done
+  wait
 }
 
 function print_stats() {
-  pl_g "===== total ================================================"
+  pbl_c "===== total ================================================"
   jq -s add pullrequests/*.json | jq -r '. | length'
 
-  pl_g "===== per state ============================================"
+  pbl_c "===== per state ============================================"
   printf "%-6s %-6s\n" "state" "count"
   printf "====== ======\n"
   jq -s add pullrequests/*.json | jq -r 'group_by(.state) | map({"state": .[0].state, "count": [.[]]|length}) | .[] | [.state,.count] | @tsv' \
     | sort -rd \
     | awk -F"\t" '{ printf "%-6s %6s\n", $1, $2; }'
 
-  pl_g "===== per repository ======================================="
-  printf "%-25s %6s %6s\n" "repository name" "merged" "closed"
-  printf "========================= ====== ======\n"
+  pbl_c "===== per repository ======================================="
+  printf "%-25s %6s %6s %9s %9s\n" "repository name" "merged" "closed" "additions" "deletions"
+  printf "========================= ====== ====== ========= =========\n"
   jq -s add pullrequests/*.json \
-    | jq -r 'group_by(.repo) | map({ "repo": .[0].repo, "total":  [.[]]|length, "open": [.[]|select(.state=="OPEN")]|length, "merged": [.[]|select(.state=="MERGED")]|length, "closed": [.[]|select(.state=="CLOSED")]|length }) | .[] | [.repo,.merged,.closed] | @tsv' \
-    | awk -F"\t" '{ printf "%-25s %6s %6s\n", $1, $2, $3; }'
+    | jq -r 'group_by(.repo) | map({ "repo": .[0].repo, "total":  [.[]]|length, "open": [.[]|select(.state=="OPEN")]|length, "merged": [.[]|select(.state=="MERGED")]|length, "closed": [.[]|select(.state=="CLOSED")]|length, "additions": [.[].additions]|add, "deletions": [.[].deletions]|add }) | .[] | [.repo,.merged,.closed,.additions,.deletions] | @tsv' \
+    | awk -F"\t" '{ printf "%-25s %6d %6d \x1b[32m%9d\x1b[0m \x1b[31m%9d\x1b[0m\n", $1, $2, $3, $4, $5; }'
 
-  pl_g "===== per year ============================================="
-  printf "%-5s %6s %6s\n" "year" "merged" "closed"
-  printf "===== ====== ======\n"
+  pbl_c "===== per year ============================================="
+  printf "%-5s %6s %6s %9s %9s\n" "year" "merged" "closed" "additions" "deletions"
+  printf "===== ====== ====== ========= =========\n"
   jq -s add pullrequests/*.json \
-    | jq -r '[.[]|select(.state!="OPEN")]|map({"year":.closedAt|fromdate|strftime("%Y"),"state":.state,"repo":.repo})|group_by(.year)|map({"year":.[0].year, "merged":[.[]|select(.state=="MERGED")]|length, "closed":[.[]|select(.state=="CLOSED")]|length}) | .[] | [.year,.merged,.closed] | @tsv' \
-    | awk -F"\t" '{ printf "%-5s %6s %6s\n", $1, $2, $3; }'
+    | jq -r '[.[]|select(.state!="OPEN")]|map({"year":.closedAt|fromdate|strftime("%Y"),"state":.state,"repo":.repo,"additions":.additions,"deletions":.deletions})|group_by(.year)|map({ "year":.[0].year, "merged":[.[]|select(.state=="MERGED")]|length, "closed":[.[]|select(.state=="CLOSED")]|length, "additions": [.[].additions]|add, "deletions": [.[].deletions]|add }) | .[] | [.year,.merged,.closed,.additions,.deletions] | @tsv' \
+    | awk -F"\t" '{ printf "%-5s %6d %6d \x1b[32m%9d\x1b[0m \x1b[31m%9d\x1b[0m\n", $1, $2, $3, $4, $5; }'
 }
 
 function main() {
