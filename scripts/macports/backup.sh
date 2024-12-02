194559b007c21b31d9c3b4b6f48788d0d538f7e1
shkumagai <take.this.2.your.grave@gmail.com>
2024-12-02 09:34:54 +0900

    refactoring scripts for macports

--- scripts/macports/backup.sh
+++ scripts/macports/backup.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-readonly red='0;31'
 readonly green='0;32'
 readonly brown='0;33'
-readonly blue='0;34'
-readonly gray='0;37'
 readonly org='0;0'
+
 
 clr_echo ()
 {
   msg=${1}
-  color=${2:-$(echo $org)}
-  echo -e "\033[${color}m${msg}\033[${org}m"
+  color=${2:-"${org}"}
+  printf "\033[${color}m%s\033[${org}m\n" "${msg}"
 }
 
-
-CWD=$(cd $(dirname ${0}); pwd)
+# shellcheck disable=SC2164
+CWD=$(cd "$(dirname "${0}")"; pwd)
 
 function main() {
-  clr_echo "Backup installed ports..." $brown
+  clr_echo "Backup installed ports..." "${brown}"
 
-  port -qv installed > ${CWD}/myports.txt
-  port echo requested | cut -d ' ' -f 1 | uniq > ${CWD}/requested.txt
+  port -qv installed > "${CWD}/myports.txt"
+  port echo requested | cut -d ' ' -f 1 | uniq > "${CWD}/requested.txt"
 
-  clr_echo "Done" $brown
+  clr_echo "Done" "${brown}"
 }
 
 
-main ${@}
+main "${@}"
 
 # END
--- scripts/macports/restore.sh
+++ scripts/macports/restore.sh
@@ -1,38 +1,36 @@
 #!/bin/bash
 
-readonly red='0;31'
 readonly green='0;32'
 readonly brown='0;33'
-readonly blue='0;34'
-readonly gray='0;37'
 readonly org='0;0'
+
 
 clr_echo ()
 {
   msg=${1}
-  color=${2:-$(echo $org)}
-  echo -e "\033[${color}m${msg}\033[${org}m"
+  color=${2:-"${org}"}
+  printf "\033[${color}m%s\033[${org}m\n" "${msg}"
 }
 
-
-CWD=$(cd $(dirname ${0}); pwd)
+# shellcheck disable=SC2164
+CWD=$(cd "$(dirname "${0}")"; pwd)
 
 function main() {
-  clr_echo "Restore installed ports..." $brown
+  clr_echo "Restore installed ports..." "${brown}"
 
   sudo port -f uninstall installed
   curl --location --remote-name "https://github.com/macports/macports-contrib/raw/master/restore_ports/restore_ports.tcl"
-  chmod +x ${CWD}/restore_ports.tcl
+  chmod +x "${CWD}"/restore_ports.tcl
   # xattr -d com.apple.quarantine restore_ports.tcl
-  sudo ${CWD}/restore_ports.tcl ${CWD}/myports.txt
+  sudo "${CWD}/restore_ports.tcl ${CWD}/myports.txt"
 
   sudo port unsetrequested installed
-  xargs sudo port setrequested < ${CWD}/requested.txt
+  xargs sudo port setrequested < "${CWD}/requested.txt"
 
-  clr_echo "Done" $brown
+  clr_echo "Done" "${brown}"
 }
 
 
-main ${@}
+main "${@}"
 
 # END
