60b1e7591d61f874432f98d369de090940c92bba
shkumagai <take.this.2.your.grave@gmail.com>
2026-04-09 08:47:38 +0900

    add Environment variables to use Claude code

--- zsh.d/zshenv-darwin.sh
+++ zsh.d/zshenv-darwin.sh
@@ -77,7 +77,7 @@ fi
 
 # Google Cloud SDK
 source "$(mise where gcloud)/completion.zsh.inc"
-CLOUDSDK_PYTHON=/opt/local/bin/python3.12
+CLOUDSDK_PYTHON=/opt/local/bin/python3.13
 export CLOUDSDK_PYTHON
 GOOGLE_CLOUD_PROJECT="private-shoji-kumagai-01"
 GOOGLE_CLOUD_LOCATION="us-central1"
@@ -92,8 +92,13 @@ export ANTHROPIC_MODEL="claude-opus-4-6"
 # オプション：必要に応じてプロンプトキャッシングを無効にする
 export DISABLE_PROMPT_CACHING=1
 
+# Pinning AI models
+export ANTHROPIC_DEFAULT_OPUS_MODEL="claude-opus-4-6"
+export ANTHROPIC_DEFAULT_SONNET_MODEL="claude-sonnet-4-6"
+
 # CLOUD_ML_REGION=global の場合、サポートされていないモデルのリージョンをオーバーライドする
 # export VERTEX_REGION_CLAUDE_3_5_HAIKU=us-east5
+export VERTEX_REGION_CLAUDE_4_6_SONNET=global
 
 # Local variables:
 # mode: shell-script
